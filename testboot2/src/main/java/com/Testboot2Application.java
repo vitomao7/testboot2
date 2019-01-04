0d34b65a5347c2478d943068998b5d97d6e7ad69
vitomao7 <1843443923@qq.com>
2019-01-04 11:47:08 +0800

    创建test2

---
+++ testboot2/src/main/java/com/Testboot2Application.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testboot2Application.class, args);
+	}
+
+}
+
